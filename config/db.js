9c81214ef752cff6e5829ad76dd920b5278eee65
Zabounet <hkynuggggg@gmail.com>
2025-09-10 00:53:32 +0200

    initial commit

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ connectDB().catch(err => console.log(err));
 
 async function connectDB() {
   await mongoConnect.connect(
-    "mongodb+srv://hkynuggggg_db_user:alorsOnDanse@bros-vouchers.sgzbjuy.mongodb.net/?retryWrites=true&w=majority&appName=bros-vouchers",
-    { dbName: "bros-vouchers" }
+    process.env.MONGO_URI,
+    { dbName: process.env.DB_NAME }
   );
 }
 
